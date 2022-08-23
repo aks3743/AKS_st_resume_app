8c6e8b08c9888249b583349d01037475c4a11737
ABHIJITH K S <45867750+aks3743@users.noreply.github.com>
2022-08-24 02:06:32 +0530

    new update

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def main():
         "This an interactive streamlit app completely created with Python's latest library **streamlit** "
         "Do reach out to me on [LinkedIn](https://www.linkedin.com/in/abhijith-k-s-aks3743/) or "
         "at [Mail me](mailto:abhishek.2.gupta@uconn.edu) to know more. "
-        "Also check the [source code](https://github.com/alphadatagamma/Streamlit-Resume-App) here. "  
+        "Also check the [source code](https://github.com/aks3743/AKS_st_resume_app) here. "  
 
 )
 
