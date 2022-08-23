12ce75dfa71e7209fbe3a8c327f3360568d8a330
ABHIJITH K S <45867750+aks3743@users.noreply.github.com>
2022-08-24 05:20:08 +0530

    final change for today

--- app.py
+++ app.py
@@ -27,17 +27,17 @@ def main():
         ast.write_page(page)
 
     st.sidebar.title("Hire Me")
-    st.sidebar.info(
-        """
-        If you are looking to hire a Data Scientist, 
-        [email me](mailto:aksstudy94@gmail.com) or reach out 
-        to me on [Linkedin](https://www.linkedin.com/in/abhijith-k-s-aks3743/)
-""")
-    st.sidebar.title("Additional Info")
+#     st.sidebar.info(
+#         """
+#         If you are looking to hire a Data Scientist, 
+#         [email me](mailto:aksstudy94@gmail.com) or reach out 
+#         to me on [Linkedin](https://www.linkedin.com/in/abhijith-k-s-aks3743/)
+# """)
+    # st.sidebar.title("Additional Info")
     st.sidebar.info(
         "This an interactive streamlit app completely created with Python's latest library **streamlit** "
         "Do reach out to me on [LinkedIn](https://www.linkedin.com/in/abhijith-k-s-aks3743/) or "
-        "at [Mail me](mailto:abhishek.2.gupta@uconn.edu) to know more. "
+        "at [Mail me](mailto:aksstudy94@gmail.com) to know more. "
         "Also check the [source code](https://github.com/aks3743/AKS_st_resume_app) here. "  
 
 )
