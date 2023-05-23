d5a32b91bd84524bf998501562d38f2613b63ebf
Abhijith <aks3743@gmail.com>
2023-05-23 23:50:26 +0100

    files renamed to support the new features in streamlit

---
+++ Intro.py
@@ -0,0 +1,62 @@
+import streamlit as st
+
+
+st.sidebar.title("Hire Me")
+
+st.sidebar.info(
+        """
+        If you are looking to hire a Data Scientist, 
+        [email me](mailto:aksstudy94@gmail.com) or reach out 
+        to me on [Linkedin](https://www.linkedin.com/in/abhijith-k-s-aks3743/)
+""")
+st.sidebar.title("Additional Info")
+st.sidebar.info(
+        "This an interactive streamlit app completely created with Python's latest library **streamlit** "
+        "Do reach out to me on [LinkedIn](https://www.linkedin.com/in/abhijith-k-s-aks3743/) or "
+        "at [Mail me](mailto:aksstudy94@gmail.com) to know more. "
+        "Also check the [source code](https://github.com/aks3743/AKS_st_resume_app) here. "  )
+
+st.write('''
+
+<h1 align="center">Hi 👋, I'm Abhijith K S</h1>
+<h3 align="center">A passionate Data and Decision Scientist from Ireland</h3>
+
+<p align="left"> <img src="https://komarev.com/ghpvc/?username=aks3743&label=Profile%20views&color=0e75b6&style=flat" alt="aks3743" /> </p>
+
+<p align="left"> <a href="https://github.com/ryo-ma/github-profile-trophy"><img src="https://github-profile-trophy.vercel.app/?username=aks3743" alt="aks3743" /></a> </p>
+
+- 🔭 I’m currently working on **Natural Language Processing**
+
+- 🌱 I’m currently learning **NLP, DL**
+
+- 👯 I’m looking to collaborate on **NLP projects, Computer vision**
+
+- 🤝 I’m looking for help with **Computer vision**
+
+- 👨‍💻 All of my projects are available at [https://aks3743-aks-st-resume-app-app-y0rc12.streamlitapp.com/](https://aks3743-aks-st-resume-app-app-y0rc12.streamlitapp.com/)
+
+- 💬 Ask me about **NLP, DL**
+
+- 📫 How to reach me **aksstudy94@gmail.com**
+
+- 📄 Know about my experiences [https://aks3743-aks-st-resume-app-app-y0rc12.streamlitapp.com/](https://aks3743-aks-st-resume-app-app-y0rc12.streamlitapp.com/)
+
+<h3 align="left">Connect with me:</h3>
+<p align="left">
+<a href="https://linkedin.com/in/abhijith-k-s-aks3743/" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="abhijith-k-s-aks3743/" height="30" width="40" /></a>
+<a href="https://kaggle.com/aks3743/" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/kaggle.svg" alt="aks3743/" height="30" width="40" /></a>
+</p>
+
+<h3 align="left">Languages and Tools:</h3>
+<p align="left"> <a href="https://www.arduino.cc/" target="_blank" rel="noreferrer"> <img src="https://cdn.worldvectorlogo.com/logos/arduino-1.svg" alt="arduino" width="40" height="40"/> </a> <a href="https://www.gnu.org/software/bash/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/gnu_bash/gnu_bash-icon.svg" alt="bash" width="40" height="40"/> </a> <a href="https://www.docker.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg" alt="docker" width="40" height="40"/> </a> <a href="https://flask.palletsprojects.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/pocoo_flask/pocoo_flask-icon.svg" alt="flask" width="40" height="40"/> </a> <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40"/> </a> <a href="https://hadoop.apache.org/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/apache_hadoop/apache_hadoop-icon.svg" alt="hadoop" width="40" height="40"/> </a> <a href="https://hive.apache.org/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/apache_hive/apache_hive-icon.svg" alt="hive" width="40" height="40"/> </a> <a href="https://www.linux.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg" alt="linux" width="40" height="40"/> </a> <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" width="40" height="40"/> </a> <a href="https://www.microsoft.com/en-us/sql-server" target="_blank" rel="noreferrer"> <img src="https://www.svgrepo.com/show/303229/microsoft-sql-server-logo.svg" alt="mssql" width="40" height="40"/> </a> <a href="https://www.mysql.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="40" height="40"/> </a> <a href="https://opencv.org/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/opencv/opencv-icon.svg" alt="opencv" width="40" height="40"/> </a> <a href="https://pandas.pydata.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/2ae2a900d2f041da66e950e4d48052658d850630/icons/pandas/pandas-original.svg" alt="pandas" width="40" height="40"/> </a> <a href="https://www.postgresql.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" alt="postgresql" width="40" height="40"/> </a> <a href="https://www.python.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a> <a href="https://pytorch.org/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/pytorch/pytorch-icon.svg" alt="pytorch" width="40" height="40"/> </a> <a href="https://scikit-learn.org/" target="_blank" rel="noreferrer"> <img src="https://upload.wikimedia.org/wikipedia/commons/0/05/Scikit_learn_logo_small.svg" alt="scikit_learn" width="40" height="40"/> </a> <a href="https://seaborn.pydata.org/" target="_blank" rel="noreferrer"> <img src="https://seaborn.pydata.org/_images/logo-mark-lightbg.svg" alt="seaborn" width="40" height="40"/> </a> <a href="https://www.sqlite.org/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/sqlite/sqlite-icon.svg" alt="sqlite" width="40" height="40"/> </a> <a href="https://www.tensorflow.org" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/tensorflow/tensorflow-icon.svg" alt="tensorflow" width="40" height="40"/> </a> </p>
+
+<h3 align="left">Support:</h3>
+<p><a href="https://www.buymeacoffee.com/aksstudy946"> <img align="left" src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" height="50" width="210" alt="aksstudy946" /></a></p><br><br>
+
+<p><img align="left" src="https://github-readme-stats.vercel.app/api/top-langs?username=aks3743&show_icons=true&locale=en&layout=compact" alt="aks3743" /></p>
+
+<p>&nbsp;<img align="center" src="https://github-readme-stats.vercel.app/api?username=aks3743&show_icons=true&locale=en" alt="aks3743" /></p>
+
+<p><img align="center" src="https://github-readme-streak-stats.herokuapp.com/?user=aks3743&" alt="aks3743" /></p>
+''', unsafe_allow_html=True
+)
